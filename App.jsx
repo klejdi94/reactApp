7bbf113df71e3905df294144f2fdacaa038cf6cc
Kristo Klejdi <k.kristo@almaviva.it>
2018-02-08 09:08:42 +0100

    fix

--- App.jsx
+++ App.jsx
@@ -15,22 +15,17 @@ class App extends React.Component {
 }
 class Header extends React.Component {
     render() {
-        return ( <
-            div >
-            <
-            h1 > Header < /h1> <
-            /div>
+        return ( <div >
+            < h1 > Header </h1> </div>
         );
     }
 }
 class Content extends React.Component {
     render() {
-        return ( <
-            div >
-            <
-            h2 > Content < /h2> <
-            p > The content text!!! < /p> <
-            /div>
+        return ( <div>
+            <h2 > Content </h2> 
+            <p> The content text!!! </p> 
+            </div>
         );
     }
 }
